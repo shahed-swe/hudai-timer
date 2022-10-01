becc78af1b4d6efb61eca5dd004b6ea1f1ce282f
Md Shahed Talukder <shahedtalukder51@gmail.com>
2022-10-01 10:35:24 +0600

    new commity

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [hour, setHour] = useState()
+  const [minute, setMinute] = useState()
+  const [second, setSecond] = useState()
+  const [timer, setTimer] = useState()
+
+
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTimer(new Date().getSeconds() - 60)
+      setSecond(new Date().getSeconds())
+      setMinute(new Date().getMinutes())
+      setHour(new Date().getHours())
+    }, 1000)
+    return () => clearInterval(interval)
+  })
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p style={{ margin: "0" }}>Time <span style={{ textDecoration: "underline" }}>{hour}:{minute}:{second}</span></p>
+        Time Running Out ({timer * -1})
       </header>
     </div>
   );
